73e2dcf026382119676d427d4bd108e3ef263ab0
Antoine Colmard <a.colmard@gmail.com>
2020-07-29 20:27:47 +0200

    :recycle: Use emotion Global for global styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 /** @jsx jsx */
-import { jsx } from "@emotion/core";
+import { Global, jsx } from "@emotion/core";
 import { StrictMode } from "react";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { I18nextProvider } from "react-i18next";
@@ -9,26 +9,25 @@ import Home from "./Home/Home";
 import i18n from "./i18n";
 import NotFound from "./NotFound/NotFound";
 
-function App() {
+const App = () => {
   return (
     <StrictMode>
-      <div css={tw`antialiased font-sans`}>
-        <I18nextProvider i18n={i18n}>
-          <HelmetProvider>
-            <Helmet titleTemplate="%s | React App" />
-            <Router>
-              <Switch>
-                <Route path="/" exact>
-                  <Home />
-                </Route>
-                <Route path="*" exact>
-                  <NotFound />
-                </Route>
-              </Switch>
-            </Router>
-          </HelmetProvider>
-        </I18nextProvider>
-      </div>
+      <Global styles={{ body: tw`antialiased font-sans` }} />
+      <I18nextProvider i18n={i18n}>
+        <HelmetProvider>
+          <Helmet titleTemplate="%s | React App" />
+          <Router>
+            <Switch>
+              <Route path="/" exact>
+                <Home />
+              </Route>
+              <Route path="*" exact>
+                <NotFound />
+              </Route>
+            </Switch>
+          </Router>
+        </HelmetProvider>
+      </I18nextProvider>
     </StrictMode>
   );
 }
