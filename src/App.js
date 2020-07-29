ec15afca1ffd35804ef5e6e7e7da74774a8c517b
Antoine Colmard <a.colmard@gmail.com>
2020-07-29 11:06:38 +0200

    :sparkles: Add tailwind + twin styling
    Co-authored-by: Guilmain Dorian <guilmain.dorian@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import React from "react";
+/** @jsx jsx */
+import { jsx } from "@emotion/core";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import tw from "twin.macro";
 import Home from "./Home/Home";
 import NotFound from "./NotFound/NotFound";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" exact>
-          <Home />
-        </Route>
-        <Route path="*" exact>
-          <NotFound />
-        </Route>
-      </Switch>
-    </Router>
+    <div css={tw`antialiased font-sans`}>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="*" exact>
+            <NotFound />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
