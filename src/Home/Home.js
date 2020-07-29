548ac882a00a290af32499b77a43a65bf9ad7ff7
Antoine Colmard <a.colmard@gmail.com>
2020-07-30 00:30:48 +0200

    :fire: Remove test button

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -343,9 +343,7 @@ export default function Home() {
       <main tw="max-w-7xl mx-auto -mt-32 px-4 sm:px-6 lg:px-8 pb-12">
         <section tw="bg-white rounded-lg shadow px-5 py-6 sm:px-6">
           {/* Replace with your content */}
-          <div tw="border-4 border-dashed border-gray-200 rounded-lg h-96">
-            <button>Button</button>
-          </div>
+          <div tw="border-4 border-dashed border-gray-200 rounded-lg h-96" />
         </section>
       </main>
     </Fragment>
