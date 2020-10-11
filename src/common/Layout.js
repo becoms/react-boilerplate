600b2b1f22e44cc910b8a90320693ed8b6e53b19
Antoine Colmard <a.colmard@gmail.com>
2020-10-11 23:26:45 +0200

    🔎 Add image width and height

--- src/common/Layout.js
+++ src/common/Layout.js
@@ -64,6 +64,8 @@ const ProfileDropdown = () => {
                 tw="h-8 w-8 rounded-full"
                 src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                 alt=""
+                width={256}
+                height={256}
               />
             </MenuButton>
             {/* Profile dropdown panel, show/hide based on dropdown state. */}
