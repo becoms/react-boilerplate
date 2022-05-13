1ec44e22db12d0a58c9ab1faa90ddc069b81beb6
ben <bcaure@gmail.com>
2022-05-13 16:55:59 +0200

    :bug: Add error message if import fails on file read

--- src/shared/CrudImport.js
+++ src/shared/CrudImport.js
@@ -1,63 +1,39 @@
 /** @jsxImportSource @emotion/react */
-import { DocumentAddIcon } from "@heroicons/react/solid";
+import { useEffect, useMemo, useState } from "react";
 import { useId } from "@reach/auto-id";
-import { useMutation } from "react-query";
 import "twin.macro";
-import { LoadingIndicator } from "../shared/LoadingIndicator";
-import { Button } from "../shared/Buttons";
+import { readExcelFile } from "../functions/Import";
+import { Alert } from "./Modal";
+import { ImportButton } from "./Buttons";
+import { handleError } from "../functions/ErrorHandling";
 
 /**
  * Fields is the list of columns to import.
  * FieldsProcessors is the list of function (matching fields index) to apply to each cell value
  */
-export const CrudImport = ({ fieldNames, fieldsProcessors, upsertItem }) => {
-  const defaultProcessor = (val) => val;
-  const { mutateAsync: importFile, status } = useMutation(
-    async (file) => {
-      const { default: ExcelJS } = await import("exceljs");
-      const workbook = new ExcelJS.Workbook();
-      await workbook.xlsx.load(file);
-      const worksheet = workbook.getWorksheet(1);
-      const items = [];
-      worksheet.eachRow({ includeEmpty: true }, async (row, rowNumber) => {
-        if (rowNumber !== 1) { // ignore header
-          const item = {};
-          for (let i = 0; i < fieldNames.length; i++) {
-            const field = fieldNames[i];
-            const fieldProcessors = fieldsProcessors[i] ?? defaultProcessor;
-            const value = fieldProcessors(row.getCell(i + 1).value);
-            item[field] = value;
-          }
-          items.push(item);
-        }
-      });
-
-      for (const item of items) {
-        try {
-          await upsertItem(item);
-        } catch (error) {
-          console.error("Failed to insert item", item, error);
-        }
-      }
-    },
-    {
-      onMutate: () => {
-        console.log("Importing items to the app...");
-        console.log("Importing items from the XLSX file to the app");
-      },
-      onSuccess: () => {
-        console.log("Items imported!");
-        console.log("Items are imported successfully");
-      },
-    }
-  );
+export const CrudImport = ({ fieldNames, fieldsProcessors, useUpsertManyMutation }) => {
+  const [message, setMessage] = useState(undefined);
+  const { mutateAsync: upsertItems, status: upsertStatus, error: upsertError } = useUpsertManyMutation(false);
+  const [readStatus, setReadStatus] = useState(undefined);
 
   const id = useId();
 
   const handleImportFile = async (e) => {
     const file = e.target.files[0];
-    importFile(file);
+    try {
+      const items = await readExcelFile(file, fieldNames, fieldsProcessors);
+      upsertItems(items);
+    } catch (err) {
+      setReadStatus("error");
+      setMessage(`${message}\n${err}`);
+    }
   };
+
+  const globalStatus = useMemo(() => readStatus === "success" ? upsertStatus : readStatus, [upsertStatus, readStatus]);
+  useEffect(() => {
+    handleError(upsertError).then((errMsg) => setMessage(errMsg));
+    console.log(upsertError);
+  }, [upsertError]);
 
   return (
     <>
@@ -65,13 +41,13 @@ export const CrudImport = ({ fieldNames, fieldsProcessors, upsertItem }) => {
         type="file"
         tw="sr-only"
         id={id}
-        disabled={status === "loading"}
+        disabled={globalStatus === "loading"}
         onChange={handleImportFile}
       />
-      <Button as="label" tw="cursor-pointer" disabled={status === "loading"} htmlFor={id}>
-        {status === "loading" ? <LoadingIndicator tw="h-5 w-5" /> : <DocumentAddIcon tw="h-5 w-5" />}
+      <ImportButton as="label" status={globalStatus} htmlFor={id}>
         Importer
-      </Button>
+      </ImportButton>
+      <Alert message={message} setMessage={setMessage} />
     </>
   );
 };
